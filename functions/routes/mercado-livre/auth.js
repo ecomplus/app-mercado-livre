5c58c704354e799794455115986bd838738a1a6e
Thiago Abreu Rodrigues <thiagoabreu.dev@gmail.com>
2020-06-17 00:11:39 -0300

    chore: fix instance meli

--- functions/routes/mercado-livre/auth.js
+++ functions/routes/mercado-livre/auth.js
@@ -1,9 +1,9 @@
-const qs = require('qs')
 const meli = require('mercadolibre')
 exports.get = ({ appSdk }, req, res) => {
   const { code } = req.query
-  const redirect_uri = meli.getAuthURL()
-  meli.authorize(code, redirect_uri, (err, result) => {
+  const meliObject = new meli.Meli('6653886911586901', '7D1OvA7YYK35p5EcX9rz00HsAMACjdGL')
+  const redirect_uri = meliObject.getAuthURL()
+  meliObject.authorize(code, redirect_uri, (err, result) => {
     return res.json(err, result)
   })
 
