6653eade5a91d159d512609f7cb5d1ee4e2a5a8e
Thiago Abreu Rodrigues <thiagoabreu.dev@gmail.com>
2020-06-17 00:16:50 -0300

    chore: adjusted return

--- functions/routes/mercado-livre/auth.js
+++ functions/routes/mercado-livre/auth.js
@@ -1,10 +1,14 @@
 const meli = require('mercadolibre')
 exports.get = ({ appSdk }, req, res) => {
-  const { code } = req.query
-  const meliObject = new meli.Meli('6653886911586901', '7D1OvA7YYK35p5EcX9rz00HsAMACjdGL')
-  const redirect_uri = meliObject.getAuthURL()
-  meliObject.authorize(code, redirect_uri, (err, result) => {
-    return res.json(err, result)
-  })
+  try {
+    const { code } = req.query
+    const meliObject = new meli.Meli('6653886911586901', '7D1OvA7YYK35p5EcX9rz00HsAMACjdGL')
+    const redirectUri = meliObject.getAuthURL()
+    meliObject.authorize(code, redirectUri, (err, result) => {
+      return res.json({ err, result })
+    })
+  } catch (error) {
+    return res.status(500).send(error)
+  }
 
 }
