f1ce986a1a92c3afe3a809e251619a3d962dd56e
Thiago Abreu Rodrigues <thiagoabreu.dev@gmail.com>
2020-06-16 23:39:10 -0300

    chore: adjusted params to get access_token

--- functions/routes/mercado-livre/auth.js
+++ functions/routes/mercado-livre/auth.js
@@ -3,10 +3,10 @@ const axios = require('axios')
 exports.get = ({ appSdk }, req, res) => {
   const { code } = req.query
   const params = qs.stringify({
-    granty_type: 'authorization_code',
+    grant_type: 'authorization_code',
     client_id: '6653886911586901', // Todo get to env
     client_secret: '7D1OvA7YYK35p5EcX9rz00HsAMACjdGL', // Todo get to env
-    redirect_url: 'https://us-central1-ecom-mercado-livre.cloudfunctions.net/app/mercado-livre/auth',
+    redirect_uri: 'https://us-central1-ecom-mercado-livre.cloudfunctions.net/app/mercado-livre/auth',
     code
   })
   const url = `https://api.mercadolibre.com/oauth/token?${params}`
