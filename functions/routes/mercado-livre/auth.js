eb6ffb3874d18c17e30e18addc2958121c1e3160
Thiago Abreu Rodrigues <thiagoabreu.dev@gmail.com>
2020-06-17 00:21:23 -0300

    chore: adjusted redirect_uri

--- functions/routes/mercado-livre/auth.js
+++ functions/routes/mercado-livre/auth.js
@@ -3,7 +3,7 @@ exports.get = ({ appSdk }, req, res) => {
   try {
     const { code } = req.query
     const meliObject = new meli.Meli('6653886911586901', '7D1OvA7YYK35p5EcX9rz00HsAMACjdGL')
-    const redirectUri = meliObject.getAuthURL()
+    const redirectUri = 'https://us-central1-ecom-mercado-livre.cloudfunctions.net/app/mercado-livre/auth'
     meliObject.authorize(code, redirectUri, (err, result) => {
       return res.json({ err, result })
     })
