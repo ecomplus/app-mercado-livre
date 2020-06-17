cb4e81067f569571545dd423c8624c1025e5f478
Thiago Abreu Rodrigues <thiagoabreu.dev@gmail.com>
2020-06-16 23:34:37 -0300

    chore: teste to request token ml

--- functions/routes/mercado-livre/auth.js
+++ functions/routes/mercado-livre/auth.js
@@ -1,6 +1,16 @@
+const qs = require('qs')
+const axios = require('axios')
 exports.get = ({ appSdk }, req, res) => {
-  return res.json({
-    query: req.query,
-    appSdk
+  const { code } = req.query
+  const params = qs.stringify({
+    granty_type: 'authorization_code',
+    client_id: '6653886911586901', // Todo get to env
+    client_secret: '7D1OvA7YYK35p5EcX9rz00HsAMACjdGL', // Todo get to env
+    redirect_url: 'https://us-central1-ecom-mercado-livre.cloudfunctions.net/app/mercado-livre/auth',
+    code
   })
+  const url = `https://api.mercadolibre.com/oauth/token?${params}`
+  axios.post(url)
+    .then(res => res.json(res))
+    .catch(error => res.send(error))
 }
