8c38fa34b746b462caa6a0088c520061c3337c5d
Thiago Abreu Rodrigues <thiagoabreu.dev@gmail.com>
2020-06-18 06:24:43 -0300

    chore: adjusted to save mercado livre token in firebase

--- functions/routes/mercado-livre/auth.js
+++ functions/routes/mercado-livre/auth.js
@@ -1,11 +1,20 @@
 const meli = require('mercadolibre')
-exports.get = ({ appSdk }, req, res) => {
+exports.get = ({ admin }, req, res) => {
   try {
     const { code } = req.query
     const meliObject = new meli.Meli('6653886911586901', '7D1OvA7YYK35p5EcX9rz00HsAMACjdGL')
     const redirectUri = 'https://us-central1-ecom-mercado-livre.cloudfunctions.net/app/mercado-livre/auth'
     meliObject.authorize(code, redirectUri, (err, result) => {
-      return res.json({ err, result })
+      if (err) {
+        throw new Error(err)
+      }
+      admin.firestore()
+        .collection('ml_app_auth')
+        .set(result)
+        .then(data => {
+          return res.json({ data })
+        })
+        .catch(err => { throw new Error(err) })
     })
   } catch (error) {
     return res.status(500).send(error)
