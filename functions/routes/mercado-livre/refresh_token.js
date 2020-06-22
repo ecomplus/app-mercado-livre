dab43161cda6d5f81016a9845c35d643f0e5359f
Thiago Abreu Rodrigues <thiagoabreu.dev@gmail.com>
2020-06-22 06:51:45 -0300

    chore: test to update with refrash_token

--- functions/routes/mercado-livre/refresh_token.js
+++ functions/routes/mercado-livre/refresh_token.js
@@ -1,5 +1,27 @@
-exports.post = ({ appSdk }, req, res) => {
+const meli = require('mercadolibre')
+
+exports.post = ({ admin }, req, res) => {
   try {
+    const results = []
+    admin.firestore()
+      .collection('ml_app_auth')
+      .get()
+      .then(auths => {
+        auths.forEach(auth => {
+          const { access_token, refresh_token } = auth
+          const meliObject = new meli.Meli(
+            '6653886911586901',
+            '7D1OvA7YYK35p5EcX9rz00HsAMACjdGL',
+            access_token,
+            refresh_token
+          )
+          meliObject.refreshAccessToken((error, result) => {
+            results.push({result, error})
+          })
+        })
+        return res.json(results)
+      })
+      .catch(err => res.status(500).send(err))
     return res.send('ok')
   } catch (error) {
     return res.status(500).send(error)
