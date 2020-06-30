42f4d9e7f247e66f87a0a7238587652adaf67f41
Thiago Abreu Rodrigues <thiagoabreu.dev@gmail.com>
2020-06-30 08:43:28 -0300

    chore: include attrbutes in ml-integration products

--- functions/lib/ml-integration/product.js
+++ functions/lib/ml-integration/product.js
@@ -1,14 +1,74 @@
 class MLProduct {
   constructor(ecomProduct) {
     this.ecomProduct = ecomProduct
-    console.log('[ML-PRODUCT]', this.ecomProduct)
   }
 
-  parserToML() {
-
+  get title() {
+    return this.ecomProduct.name
   }
 
-  save() { }
+  get price() {
+    return this.ecomProduct.price
+  }
+
+  get category_id() {
+    // Todo: check how to parser
+    return ''
+  }
+
+  get available_quantity() {
+    return this.ecomProduct.quantity
+  }
+
+  get buying_mode() {
+    // Todo: check how to work
+    return 'buy_it_now'
+  }
+
+  get condition() {
+    return this.ecomProduct.condition
+  }
+
+  get listing_type_id() {
+    // Todo: checko how to work
+    return 'gold_special'
+  }
+
+  get description() {
+    return { plain_text: this.ecomProduct.body_html }
+  }
+
+  get video() {
+    // Todo: check if exists video in ecom product
+    return ''
+  }
+
+  get sale_terms() {
+    // Todo: check how to work
+    return []
+  }
+
+  get picutres() {
+    return this.ecomProduct.picutres
+  }
+
+  get atrributes() {
+    // Todo: check what prodcts have
+    return [
+      {
+        id: 'BRAND',
+        value_name: "Product Brand"
+      },
+      {
+        id: 'EAN',
+        value: '7898095297749'
+      }
+    ]
+  }
+
+  save() {
+    console.log('[ML-INTEGRATION:SALVE PRODUCT]', this)
+  }
 
 }
 
