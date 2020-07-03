8525e7bbbf9306e7c1afe7e2f5dfa3501c2e35eb
Thiago Abreu Rodrigues <thiagoabreu.dev@gmail.com>
2020-07-03 08:30:36 -0300

    chore: commented called meli not to be giving error

--- functions/lib/ml-integration/product.js
+++ functions/lib/ml-integration/product.js
@@ -103,13 +103,13 @@ class MLProduct {
         }
 
         console.log('[ML-INTEGRATION:SALVE PRODUCT  ]', mlPayload, this.storeId)
-        // return resolve(mlPayload)
-        getMeliInstance(this.admin, this.storeId).then(instance => {
-          return instance.post('/items', mlPayload, (err, res) => {
-            console.log('[ML - MELI SAVE]', err, res)
-            return resolve(true)
-          })
-        }).catch((err) => { reject(err) })
+        return resolve(mlPayload)
+        // getMeliInstance(this.admin, this.storeId).then(instance => {
+        //   return instance.post('/items', mlPayload, (err, res) => {
+        //     console.log('[ML - MELI SAVE]', err, res)
+        //     return resolve(true)
+        //   })
+        // }).catch((err) => { reject(err) })
       } catch (error) {
         console.error('[ML-INTEGRATION:SALVE PRODUCT]', error)
         reject(error)
