f67687ab615f33b003965f8dfdd5956d21bb205b
Thiago Abreu Rodrigues <thiagoabreu.dev@gmail.com>
2020-10-10 23:48:46 -0300

    fix: fix mlService instance

--- functions/routes/mercado-livre/notification.js
+++ functions/routes/mercado-livre/notification.js
@@ -8,12 +8,9 @@ const ECHO_API_ERROR = 'STORE_API_ERR'
 
 
 exports.post = async ({ appSdk }, req, res) => {
-  // let mlService
+  let mlService
   let notification = req.body
   try {
-    // if (notification.topic !== 'orders_v2') {
-    //   return res.send(ECHO_SUCCESS)
-    // }
     mlService = await getMlService(false, notification.user_id)
     if (await mlService.hasNotification(notification)) {
       return res.status(422).send('processing for this resource already exists')
