7c5b4e43d468c6c886b36c487af119cd13f05a7a
Thiago Abreu Rodrigues <thiagoabreu.dev@gmail.com>
2020-10-31 12:51:26 -0300

    fix: test

--- functions/services/ecom_to_ml/productService.js
+++ functions/services/ecom_to_ml/productService.js
@@ -369,9 +369,9 @@ class ProductService {
     return new Promise((resolve, reject)=> {
       this.product = {}
       this.findProduct(mlProductId)
-        .then(this.buildPrice.bind(this))
+        .then(this.buildPrice)
         .then((data) => this.buildVariations(data.category_id))
-        .then(this.buildAvailableQuantity.bind(this))
+        .then(this.buildAvailableQuantity)
         .then(() => resolve(this.product))
         .catch(error => reject(error))
     })
