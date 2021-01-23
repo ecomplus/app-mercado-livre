fc069f8b6612c04641c6fb110cb1a7f83d628b6d
Thiago Abreu Rodrigues <thiagoabreu.dev@gmail.com>
2021-01-23 02:38:17 -0300

    fix: fix update profile

--- functions/routes/mercado-livre/auth.js
+++ functions/routes/mercado-livre/auth.js
@@ -4,13 +4,37 @@ const ProfileService = require('../../services/ml_to_ecom/profileService')
 const { logger } = require('firebase-functions');
 
 
+const updateProfile = (appSdk, store_id, user) => {
+  return new Promise((resolve, reject) => {
+    const profileService = new ProfileService(appSdk, store_id)
+    logger.info('[ML AUTH: START UPDATE USER INFO]', user.data())
+    if (user && user.data().access_token) {
+      return profileService.updateUserInfo(user.data())
+        .then(({ response }) => {
+          if (response && response.status == 204) {
+            logger.info('[SUCCESS TO UPDATE USER INFO]', user.data())
+            return resolve()
+          }
+          logger.error('[ML AUTH: ERROR TO UPDATE USER INFO]', response)
+          return reject('Error to process login with Mercado Livre!')
+        })
+        .catch((err) => {
+          logger.error('[ML AUTH: ERROR TO HANDLE updateUserInfo]', err)
+          return reject(err)
+        })
+    }
+    return reject('Error to proccess login with Mercado Livre')
+  })
+}
+
 exports.get = ({ admin, appSdk }, req, res) => {
   try {
     const { code } = req.query
     const meliObject = new meli.Meli(ml.client_id, ml.secret_key)
     const redirectUri = ml.redirect_uri
-    const { store_id } = req.cookies
-    const profileService = new ProfileService(appSdk, store_id)
+    // const { store_id } = req.cookies
+    var store_id = 1117
+
     meliObject.authorize(code, redirectUri, (err, result) => {
       if (err) {
         logger.error(err)
@@ -23,29 +47,11 @@ exports.get = ({ admin, appSdk }, req, res) => {
           .collection('ml_app_auth')
           .doc(`${store_id}`)
           .get()
-          .then(user => {
-            logger.info('[ML AUTH: START UPDATE USER INFO]', user.data())
-            if (user && user.data().access_token) {
-              return profileService.updateUserInfo(user.data())
-                .then(({ response }) => {
-                  if (response && response.status == 204) {
-                    logger.info('[SUCCESS TO UPDATE USER INFO]', user.data())
-                    return res.send('loading...')
-                  }
-                  logger.error('[ML AUTH: ERROR TO UPDATE USER INFO]', response)
-                  return res.send('Error to process login with Mercado Livre!')
-                })
-                .catch((err) => {
-                  logger.error('[ML AUTH: ERROR TO HANDLE updateUserInfo]', err)
-                })
-            }
-            logger.error('[ML AUTH: ML STATUS IS 400 BUT NOT HAVE USER]', result)
-            Promise.reject(new Error('Error to proccess login with Mercado Livre!'))
-          })
-          .catch((err) => {
-            logger.error(`[ML AUTH: document with store_id ${store_id} not found in ml_app_auth]`)
-          })
+          .then(user => updateProfile(appSdk, store_id, user))
+          .then(() => res.send('loading...'))
+          .catch(error => res.status(500).send(error))
       }
+
       logger.info('[ML AUTH: RESULT]', result)
 
       if (result && result.access_token) {
@@ -59,17 +65,10 @@ exports.get = ({ admin, appSdk }, req, res) => {
           .collection('ml_app_auth')
           .doc(`${store_id}`)
           .set(authData)
-          .then(({ response }) => {
-            if (response && response.status == 204) {
-              logger.info('[SUCCESS TO UPDATE USER INFO]', authData)
-              return res.send('loading...')
-            }
-            logger.error('[ML AUTH: ERROR TO UPDATE USER INFO]', response)
-            return res.send('Error to process login with Mercado Livre!')
-          })
-          .catch((err) => logger.error(err))
+          .then(user => updateProfile(appSdk, store_id, user))
+          .then(() => res.send('loading...'))
+          .catch(error => res.status(500).send(error))
       }
-
       return res.status(500).send('Error to process login with Mercado Livre!')
     })
   } catch (error) {
