27501724ca1eee738e44fae0735f7f081b679172
Thiago Abreu Rodrigues <thiagoabreu.dev@gmail.com>
2021-02-19 23:51:33 -0300

    chore: adjusted cron expression to update ml token

--- functions/index.js
+++ functions/index.js
@@ -149,7 +149,7 @@ exports.updateTokens = functions.pubsub.schedule(cron).onRun(() => {
 })
 console.log(`-- Sheduled update E-Com Plus tokens '${cron}'`)
 
-const mlCron = '30 1,3,5,7,9,13,15,17,19,21 * * *'
+const mlCron = '30 */2 * * *'
 exports.updateMLTokens = functions.pubsub.schedule(mlCron).onRun(() => {
   return updateMLTokens(admin)
 })
